00f9789cabee95647c1603b8a5363aafe4d16f5b
cyberPotions <cyberpotions@gmail.com>
2024-01-14 06:05:25 -0600

    Add quickSilver project

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -27,7 +27,46 @@ export default function Projects() {
           className="flex flex-col gap-10 items-stretch w-full justify-between py-24"
         >
           <h2 className="font-semibold text-4xl">Projects</h2>
-
+          <Card
+            isFooterBlurred
+            radius="sm"
+            fullWidth
+            isPressable
+            className="border-[0.5px] border-white/10 md:h-60 flex-col-reverse md:flex-col"
+            onClick={openAffirmProject}
+          >
+            <CardFooter className="flex flex-col gap-3 p-6 md:p-8 items-start justify-center z-10 md:w-1/2 w-full h-full bg-background/80 text-left md:absolute md:left-0 rounded-none border-t-[0.5px] md:border-t-0 md:border-r-[0.5px] border-white/10">
+              <Chip
+                size="sm"
+                className="gap-1 uppercase font-semibold tracking-wide"
+                variant="flat"
+                classNames={{
+                  base: "bg-indigo-500/20",
+                  content: "text-indigo-300",
+                }}
+                startContent={
+                  <FontAwesomeIcon
+                    icon={faPenNib}
+                    size="xs"
+                    className="ml-1 text-indigo-300"
+                  />
+                }
+              >
+                Design
+              </Chip>
+              <p className="font-bold text-2xl">quickSilver Wireframing Kit</p>
+              <p className="text-sm text-stone-300 md:pb-4">
+              quickSilver is a free wireframing kit of hand-drawn components, made to feel like you uploaded your stationery into Figma.
+              </p>
+            </CardFooter>
+            <Image
+              alt="quickSilver project cover"
+              className="object-cover h-60 z-0"
+              src="/images/quickSilver.png"
+              isZoomed
+              radius="none"
+            />
+          </Card>
           <Card
             isFooterBlurred
             radius="sm"
