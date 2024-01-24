fdbb5d4d507caa0726731e1dfde63b4a21324904
cyberPotions <cyberpotions@gmail.com>
2024-01-24 08:41:29 -0600

    Update Affirm project page

--- src/app/projects/affirm/page.tsx
+++ src/app/projects/affirm/page.tsx
@@ -6,13 +6,13 @@ import {Spinner, Code, Tooltip} from "@nextui-org/react";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-4">
-      <Section>
+      <Section id="hero">
         <Container>
           <div className="flex flex-col w-full min-h-96 gap-6 items-center justify-center">
           <Tooltip showArrow={true} content="Currently loading not shit" delay={0} closeDelay={0}>
           <Spinner size="lg" color="primary"/>
           </Tooltip>
-          <Code color="primary">The only thing actually loading is me. I'm still building this page 😭</Code>
+          <Code color="primary">The only thing actually loading is me. I&apos;m still building this page 😭</Code>
           </div>
         </Container>
       </Section>
